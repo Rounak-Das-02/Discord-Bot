5b9068f8c2378a826c2524d5a586f1390b4717cf
Rishita1712 <dasrounak2002@gmail.com>
2020-11-19 22:10:40 +0530

    driver program

---
+++ bot.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import os
+import json
+
+client = commands.Bot(command_prefix = ".")
+
+# @client.command()
+# async def load(ctx , extensions):
+#     client.load_extensions(f"cogs.{extensions}")
+
+
+
+# @client.command()
+# async def unload(ctx , extensions):
+#     client.unload_extension(f"cogs.{extensions}")
+
+
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        client.load_extension(f"cogs.{filename[:-3]}")
+
+
+@client.event
+async def on_command_error(ctx , error):
+    if isinstance(error , commands.CommandNotFound):
+        await ctx.send("Invalid Command")
+
+
+f = open(r"E:\discord bot\token.json", )
+s = json.load(f)
+
+client.run(s["Token"])
