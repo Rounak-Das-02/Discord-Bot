fb5bf79b62cc62f15d37087357ae77a55c328704
Rishita1712 <dasrounak2002@gmail.com>
2020-11-20 21:12:59 +0530

    Added some general functions

--- cogs/general.py
+++ cogs/general.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
-
+from utils.weatherapi import weatherupdate
+from utils.IndianNews import IndianNews
+from utils.NewsBBC import NewsFromBBC
 
 class general(commands.Cog):
 
@@ -19,7 +21,18 @@ class general(commands.Cog):
     async def get_owner(self , ctx):
         await ctx.channel.send(f"{self.client.appInfo.owner.mention} is my owner! ")
 
+    @commands.command(name="IndianNews")
+    async def indNews(self , ctx):
+        await ctx.channel.send(f"{IndianNews()}")
 
+    @commands.command(name = "BBCNews")
+    async def bbcnews(self , ctx):
+        await ctx.channel.send(f"{NewsFromBBC()}")
+
+    @commands.command(name = "getWeather")
+    async def weather(self ,ctx , * , message):
+        msg = str(message)
+        await ctx.channel.send(f"{weatherupdate(msg)}")
 
     ## Make APIs in weathermap , and news.
        
