9027057ac128d596a653567d2831e59541c328cb
Rishita1712 <dasrounak2002@gmail.com>
2020-11-20 21:15:49 +0530

    BBC News

---
+++ utils/NewsBBC.py
@@ -0,0 +1,30 @@
+import json
+import requests
+
+def NewsFromBBC():
+     
+    f = open(r".\tokens\token.json", )
+    s = json.load(f)
+
+    api_key = s["NewsApi"]
+
+    url = f"https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey={api_key}"
+
+    try :
+        response = requests.get(url)
+        x = response.json()
+
+        data = []
+        results = ""
+        articles = x["articles"]
+        for ar in articles :
+            data.append(ar["title"])
+        
+        for i in range(len(data)):
+            results = results + f"{i+1}. {data[i]} \n" 
+        # print (results)
+        return results
+    except:
+        # print ("Problem")
+        return "Sorry I can't fetch the data as of now .."
+
